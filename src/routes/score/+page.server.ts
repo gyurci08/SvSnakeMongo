d61ed79dfcacfa7cf1e845a52df5ca8bca25c093
gyurci08 <haver1036@gmail.com>
2024-05-01 15:00:35 +0200

    Db insert

--- src/routes/score/+page.server.ts
+++ src/routes/score/+page.server.ts
@@ -3,6 +3,6 @@ import type {PageServerLoad} from "./$types";
 
 export const load: PageServerLoad = async function() {
     const data = await scores.find({}, {projection: {'_id': 0,'name': 1, 'score': 1}}).sort({score:-1}).limit(20).toArray();
-    console.log("Data: ", data);
+    //console.log("Data: ", data);
     return { scores: data }
 }
